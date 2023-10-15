107c81e2822d2d38d027cb7650eb1f5404506139
s904842580 <904842580@qq.com>
2023-10-15 23:03:20 +0800

    vue页面

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-import ElementPlus from 'element-plus';
-import 'element-plus/theme-chalk/index.css'
+Vue.use(ElementUI);
 
-createApp(App).use(ElementPlus).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <el-container>
+    <!--  侧边栏    -->
+      <el-aside style="width: 200px; min-height: 100vh; background-color: #001529">
+        <div style="height: 60px; color: white; display: flex; align-items: center;justify-content: center">
+          夏瑾溪
+        </div>
+
+        <el-menu router background-color="#001529" text-color="rgba(255,255,255,0.65)" active-text-color="#fff" style="border: none" :default-active="$route.path"> <!--$route.path 当前浏览器访问的路由-->
+          <el-menu-item index="/">系统首页</el-menu-item>
+          <el-menu-item index="element">Element页面</el-menu-item>
+          <el-menu-item>系统首页</el-menu-item>
+          <el-menu-item>系统首页</el-menu-item>
+          <el-submenu index="">
+            <template slot="title">
+              <span>信息管理</span>
+            </template>
+            <el-menu-item>用户信息</el-menu-item>
+          </el-submenu>
+        </el-menu>
+
+      </el-aside>
+
+      <el-container>
+<!--    头部区域    -->
+        <el-header>
+
+          <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-left: 10px;margin-top: 10px">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: 'user' }">用户管理</el-breadcrumb-item>
+          </el-breadcrumb>
+
+          <div style="flex: 1; width: 0; display: flex; align-items: center; justify-content: flex-end">
+            <el-dropdown placement="bottom">
+              <img src="@/assets/real.jpg" alt="" style="width: 40px; height: 40px">
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>个人信息</el-dropdown-item>
+                <el-dropdown-item>修改密码</el-dropdown-item>
+                <el-dropdown-item @click="">退出登录</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+
+          </div>
+
+        </el-header>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
+<style>
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+.el-menu-inline{
+  background-color: #000c17 !important;
+}
+.el-menu--inline .el-menu-item{
+  background-color: #000c17 !important;
+}
+/*鼠标滑过高亮*/
+.el-menu-item:hover, .el-submenu__title:hover{
+  color: #fff !important;
+}
+/*二级标签文字滑过高亮*/
+.el-submenu__title:hover{
+  color: #fff !important;
+}
+/*背景蓝色*/
+.el-menu-item.is-active{
+  background-color: #1890ff !important;
+  border-radius: 5px !important;
+  margin: 5px !important;
+}
+/*圆角*/
+.el-menu-item{
+  height: 40px !important;
+  line-height: 40px !important;
+  margin: 5px !important;
+}
+/*阴影*/
+.el-aside{
+  box-shadow: 2px 0 6px rgba(0,21,41,.35);
+}
+/*头部阴影*/
+.el-header{
+  box-shadow: 2px 0 6px rgba(0,21,41,.35);
+  display: flex;
+  align-items: center;
+}
+</style>
