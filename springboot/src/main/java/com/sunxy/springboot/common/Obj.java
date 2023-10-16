21590cf1272cd754bf6613aa23ae1c98070c8ac2
s904842580 <904842580@qq.com>
2023-10-16 16:12:43 +0800

    vue结合Springboot完成前后端数据交互jsonshuju

---
+++ springboot/src/main/java/com/sunxy/springboot/common/Obj.java
@@ -0,0 +1,18 @@
+package com.sunxy.springboot.common;
+
+/**
+ * 功能：
+ * 作者：夏瑾溪
+ * 日期：2023/10/16 15:35
+ */
+public class Obj {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- springboot/src/main/java/com/sunxy/springboot/controller/WebController.java
+++ springboot/src/main/java/com/sunxy/springboot/controller/WebController.java
@@ -1,8 +1,8 @@
 package com.sunxy.springboot.controller;
 
+import com.sunxy.springboot.common.Obj;
 import com.sunxy.springboot.common.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 功能：提供接口返回数据
@@ -11,10 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("web")
 public class WebController {
 
-    @RequestMapping/*默认是get请求返回数据*/
-    public Result hello(){
+    @RequestMapping("/hello")
+    public Result hello(String name){/*一般都是定义对象来传递数据*/
         return Result.success("hello 夏瑾溪") ;
     }
+
+    @PostMapping("/post")
+    public Result post(@RequestBody Obj obj){
+        return Result.success(obj);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Result delete(@PathVariable Integer id){
+        return Result.success(id);
+    }
+
 }
