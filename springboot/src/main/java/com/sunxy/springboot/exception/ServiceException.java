373d685adaa4dc93dade7ae08aed4e34d48aa4a1
s904842580 <904842580@qq.com>
2023-10-20 23:20:56 +0800

    新建JwtInterceptor拦截器，override无法使用

--- springboot/src/main/java/com/sunxy/springboot/exception/ServiceException.java
+++ springboot/src/main/java/com/sunxy/springboot/exception/ServiceException.java
@@ -1,12 +1,24 @@
 package com.sunxy.springboot.exception;
 
+import lombok.Getter;
+
 /**
  * 功能：自定义异常
  * 作者：夏瑾溪
  * 日期：2023/10/19 22:20
  */
+@Getter
 public class ServiceException extends RuntimeException{
+
+    private final String code;
+
     public ServiceException(String msg) {
         super(msg);
+        this.code = "500";
+    }
+
+    public ServiceException(String code, String msg){
+        super(msg);
+        this.code = code;
     }
 }
