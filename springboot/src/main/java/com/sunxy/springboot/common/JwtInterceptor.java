06d39ad38e7ac42819bdc2865dc49d2a540b2fb7
s904842580 <904842580@qq.com>
2023-10-21 10:11:22 +0800

    解决拦截器无法使用问题

--- springboot/src/main/java/com/sunxy/springboot/common/JwtInterceptor.java
+++ springboot/src/main/java/com/sunxy/springboot/common/JwtInterceptor.java
@@ -1,10 +1,5 @@
 package com.sunxy.springboot.common;
 
-/**
- * 功能：
- * 作者：夏瑾溪
- * 日期：2023/10/20 20:57
- */
 import cn.hutool.core.util.StrUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -14,12 +9,16 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.sunxy.springboot.entity.User;
 import com.sunxy.springboot.exception.ServiceException;
 import com.sunxy.springboot.mapper.UserMapper;
-import org.springframework.web.method.HandlerMethod;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+/**
+ * 功能：自定义拦截器规则
+ * 作者：夏瑾溪
+ * 日期：2023/10/20 20:57
+ */
 
 public class JwtInterceptor implements HandlerInterceptor {
 
@@ -28,9 +27,9 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String token = request.getHeader("token");
+        String token = request.getHeader("token");//header里面传过来的参数
         if (StrUtil.isBlank(token)) {
-            token = request.getParameter("token");
+            token = request.getParameter("token");// url参数 ？token = xxxx
         }
         /*// 如果不是映射到方法直接通过
         if (handler instanceof HandlerMethod) {
@@ -46,16 +45,16 @@ public class JwtInterceptor implements HandlerInterceptor {
         // 获取 token 中的 user id
         String userId;
         try {
-            userId = JWT.decode(token).getAudience().get(0);
+            userId = JWT.decode(token).getAudience().get(0); //JWT.decode(token) 解码 jwt token
         } catch (JWTDecodeException j) {
             throw new ServiceException("401", "请登录");
         }
         // 根据token中的userid查询数据库
-        User user = userMapper.selectById(userId);
+        User user = userMapper.selectById(Integer.valueOf(userId));
         if (user == null) {
             throw new ServiceException("401", "请登录");
         }
-        // 用户密码加签验证 token
+        // 通过用户密码加密之后生成一个验证器
         JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(user.getPassword())).build();
         try {
             jwtVerifier.verify(token); // 验证token
