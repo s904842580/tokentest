8af3932a9d0d4ed6c3fa075203cc6368353a6997
s904842580 <904842580@qq.com>
2023-10-28 21:53:53 +0800

    全局的异常处理

--- springboot/src/main/java/com/sunxy/springboot/exception/GlobalException.java
+++ springboot/src/main/java/com/sunxy/springboot/exception/GlobalException.java
@@ -18,4 +18,12 @@ public class GlobalException {
     public Result serviceException(ServiceException e) {
         return Result.error(e.getCode(), e.getMessage());
     }
+
+    //全局的异常处理
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result globalException(Exception e) {
+        e.printStackTrace();
+        return Result.error("500", "系统错误");
+    }
 }
