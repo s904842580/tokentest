ddb3a6259b95fa835a62e4c957ff090148340e26
s904842580 <904842580@qq.com>
2023-10-30 22:26:24 +0800

    修改

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -8,13 +8,7 @@ const routes = [
     path: '/',
     name: 'Manager',
     component: () => import('../views/Manager'),
-    redirect: '/home', //重定向到主页
     children:[
-      {
-      path: 'home',
-      name: 'Home',
-      component: () => import('../views/manager/Home')
-    },
       {
         path: 'user',
         name: 'User',
@@ -39,20 +33,7 @@ const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
-router.beforeEach((to, from, next) => {
-  // to 是到达的路由信息
-  // from 是开源的路由信息
-  // next 是帮助我们跳转路由的函数
-  let adminPaths = ['/user']
-  let user = JSON.parse(localStorage.getItem('honey-user') || '{}')
 
-  if (user.role !== '管理员' && adminPaths.includes(to.path)) {
-    // 如果当前登录的用户不是管理员，然后当前的到达的路径是管理员才有权限访问的路径，那这个时候我就让用户去到一个没有权限的页面，不让他访问实际的页面
-    next('/403')
-  } else {
-    next()
-  }
-})
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -44,7 +44,42 @@
         </el-header>
 
         <el-main>
-           <router-view></router-view>
+          <div style="box-shadow: 0 0 10px rgba(0,0,0,.1); padding: 10px 20px; border-radius: 5px; margin-bottom: 10px">
+            早安，骚年，祝你开心每一天！
+          </div>
+          <div style="display: flex">
+            <el-card style="width: 50%; margin-right: 10px">
+              <div slot="header" class="clearfix">
+                <span>小标题</span>
+              </div>
+              <div>
+                主题内容
+                <div style="margin-top: 20px">
+                  <div style="margin: 10px 0"><strong>主题色</strong></div>
+                  <el-button type="primary">按钮</el-button>
+                  <el-button type="success">按钮</el-button>
+                  <el-button type="warning">按钮</el-button>
+                  <el-button type="danger">按钮</el-button>
+                  <el-button type="info">按钮</el-button>
+                </div>
+              </div>
+            </el-card>
+
+            <el-card style="width: 50%">
+              <div slot="header" class="clearfix">
+                <span>渲染用户的数据</span>
+                <div>
+                  <el-table :data="users">
+                    <el-table-column label="ID" prop="id"></el-table-column>
+                    <el-table-column label="用户名" prop="username"></el-table-column>
+                    <el-table-column label="姓名" prop="name"></el-table-column>
+                    <el-table-column label="地址" prop="address"></el-table-column>
+                  </el-table>
+                </div>
+
+              </div>
+            </el-card>
+          </div>
         </el-main>
 
       </el-container>
